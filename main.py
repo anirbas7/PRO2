d21bbc0ebb04008632dadc339fb94a6f86dab3be
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2021-12-06 17:53:41 +0100

    third commit

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+from flask import Flask
+from flask import render_template
+app = Flask("yazzi")
+
+@app.route("/")
+def start():
+    return render_template("start.html")
+
+@app.route('/yazzi')
+def yazzi():
+    return render_template("index.html")
 
 
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
