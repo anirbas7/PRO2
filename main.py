ff24ac231291986296b2d4a69a4d39d715e9370a
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2021-12-12 12:28:47 +0100

    fift commit

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 
 
 app = Flask("yatzy")
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def start():
-    return render_template("start.html")
+    if request.method == "POST":
+        Spieler1 = request.form.get("Spieler1")
+        return(Spieler1)
+    else:
+        return render_template("start.html")
+
 
 @app.route('/yatzy')
 def yazzi():
     return render_template("index.html")
+
+@app.route('/nav')
+def nav():
+    return render_template("navbar.html")
 
 
 if __name__ == "__main__":
