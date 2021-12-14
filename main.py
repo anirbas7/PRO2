0c55393e782a272f9dd211ab9314aeaf0130bc48
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2021-12-14 23:17:53 +0100

    sixt commit

--- main.py
+++ main.py
@@ -9,18 +9,30 @@ app = Flask("yatzy")
 def start():
     if request.method == "POST":
         Spieler1 = request.form.get("Spieler1")
-        return(Spieler1)
+        Spieler2 = request.form.get("Spieler2")
+        Spieler3 = request.form.get("Spieler3")
+        Spieler4 = request.form.get("Spieler4")
+        Spieler5 = request.form.get("Spieler5")
+        Spieler6 = request.form.get("Spieler6")
+        Spieler7 = request.form.get("Spieler7")
+        Spieler8 = request.form.get("Spieler8")
+        return(Spieler1 & Spieler2, Spieler3, Spieler4, Spieler5, Spieler6, Spieler7, Spieler8)
+
     else:
         return render_template("start.html")
 
 
 @app.route('/yatzy')
-def yazzi():
+def yatzy():
     return render_template("index.html")
 
 @app.route('/nav')
 def nav():
     return render_template("navbar.html")
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 
 if __name__ == "__main__":
