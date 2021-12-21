c6fa7a5ac3645f9adec9c164f406a1e311c656c8
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2021-12-21 21:09:34 +0100

    seventh commit

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from flask import Flask, request
 from flask import render_template
-
-
-
+import csv
 app = Flask("yatzy")
 
 @app.route("/", methods=['GET', 'POST'])
 def start():
     if request.method == "POST":
+        players =[]
         Spieler1 = request.form.get("Spieler1")
+        players.append(Spieler1)
         Spieler2 = request.form.get("Spieler2")
+        players.append(Spieler2)
         Spieler3 = request.form.get("Spieler3")
+        players.append(Spieler3)
         Spieler4 = request.form.get("Spieler4")
+        players.append(Spieler4)
         Spieler5 = request.form.get("Spieler5")
+        players.append(Spieler5)
         Spieler6 = request.form.get("Spieler6")
+        players.append(Spieler6)
         Spieler7 = request.form.get("Spieler7")
+        players.append(Spieler7)
         Spieler8 = request.form.get("Spieler8")
-        return(Spieler1 & Spieler2, Spieler3, Spieler4, Spieler5, Spieler6, Spieler7, Spieler8)
+        players.append(Spieler8)
+        print(players)
+        return render_template("index.html")
 
     else:
         return render_template("start.html")
@@ -33,6 +41,16 @@ def nav():
 @app.route('/test')
 def test():
     return render_template("test.html")
+
+@app.route('/test2')
+def test2():
+    rows = []
+    with open("8-Spieler.csv") as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        for row in csvreader:
+             rows.append(row)
+    return render_template("test2.html", ergebnise=rows, tittel=header)
 
 
 if __name__ == "__main__":
