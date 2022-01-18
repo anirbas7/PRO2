1a56fcc3bac1c7ad2ca31c8a6eb625ab9fd9737c
anirbas7 <91332683+anirbas7@users.noreply.github.com>
2022-01-18 19:27:44 +0100

    neunter commit

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
 from flask import Flask, request
 from flask import render_template
 import csv
-app = Flask("yatzy")
+app = Flask("yatzy")\
 
 @app.route("/", methods=['GET', 'POST'])
 def start():
     if request.method == "POST":
-        players =[]
-        Spieler1 = request.form.get("Spieler1")
-        players.append(Spieler1)
-        Spieler2 = request.form.get("Spieler2")
-        players.append(Spieler2)
-        Spieler3 = request.form.get("Spieler3")
-        players.append(Spieler3)
-        Spieler4 = request.form.get("Spieler4")
-        players.append(Spieler4)
-        Spieler5 = request.form.get("Spieler5")
-        players.append(Spieler5)
-        Spieler6 = request.form.get("Spieler6")
-        players.append(Spieler6)
-        Spieler7 = request.form.get("Spieler7")
-        players.append(Spieler7)
-        Spieler8 = request.form.get("Spieler8")
-        players.append(Spieler8)
-        with open("Name.csv", "w+") as file:
+        players = []
+        spieler1 = request.form.get("Spieler1")
+        players.append(spieler1)
+        spieler2 = request.form.get("Spieler2")
+        players.append(spieler2)
+        spieler3 = request.form.get("Spieler3")
+        players.append(spieler3)
+        spieler4 = request.form.get("Spieler4")
+        players.append(spieler4)
+        spieler5 = request.form.get("Spieler5")
+        players.append(spieler5)
+        spieler6 = request.form.get("Spieler6")
+        players.append(spieler6)
+        spieler7 = request.form.get("Spieler7")
+        players.append(spieler7)
+        spieler8 = request.form.get("Spieler8")
+        players.append(spieler8)
+        with open("Name.csv", "w", newline="") as file:
             writer = csv.writer(file, delimiter=";")
-            for player in towriter:
-                players.append(player)
-        return render_template("test2.html", spieler=player)
-
+            for spieler in players:
+                writer.writerow(spieler)
+        return render_template("test2.html", spieler=players)
     else:
         return render_template("start.html")
 
@@ -43,21 +42,28 @@ def nav():
 
 @app.route('/test')
 def test():
-    return render_template("test.html")
-
-@app.route('/test2')
-def test2():
     rows = []
     with open("8-Spieler.csv") as file:
         csvreader = csv.reader(file, delimiter=";")
         header = next(csvreader)
         for row in csvreader:
              rows.append(row)
-    return render_template("test2.html", ergebnise=rows, tittel=header)
+    return render_template("test.html", ergebnise=rows, tittel=header)
 
-@app.route('/test3')
-def test3():
-    return render_template("test3.html")
+@app.route('/test2')
+def test2():
+    players = []
+    rows = []
+    with open("2-Spieler.csv") as file:
+        csvreader = csv.reader(file, delimiter=";")
+        header = next(csvreader)
+        for row in csvreader:
+             rows.append(row)
+    with open("name2.csv") as file1:
+        csvnamereader = csv.reader(file1, delimiter=";")
+        for player in csvnamereader:
+            players.append(player)
+    return render_template("test2.html", ergebnise=rows, tittel=header, spieler=players)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
